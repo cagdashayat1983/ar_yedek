586a6fc03df225d7611d1ea1eb540fe1521fbc10
Çağdaş Yücedağ <cagdasyucedag@gmail.com>
2026-02-09 19:32:13 +0300

    fix: corrected podhelper path logic for codemagic

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,15 +1,16 @@
-# Bu dosya Flutter tarafından paketleri yönetmek için kullanılır.
 def flutter_root
-  generated_xcode_build_settings_path = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __FILE__)
+  # Generated.xcconfig dosyası, bu script ile aynı klasörde (ios/Flutter/) olmalı
+  generated_xcode_build_settings_path = File.expand_path('Generated.xcconfig', File.dirname(__FILE__))
+  
   unless File.exist?(generated_xcode_build_settings_path)
-    raise "#{generated_xcode_build_settings_path} must exist. If you're running pod install manually, make sure flutter pub get is executed first."
+    raise "#{generated_xcode_build_settings_path} bulunamadı. Lütfen önce 'flutter pub get' çalıştırın."
   end
 
   File.foreach(generated_xcode_build_settings_path) do |line|
     matches = line.match(/\AFLUTTER_ROOT=(.*)\z/)
     return matches[1].strip if matches
   end
-  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}. Try deleting Generated.xcconfig, then run flutter pub get"
+  raise "FLUTTER_ROOT, #{generated_xcode_build_settings_path} içinde bulunamadı."
 end
 
 require File.expand_path(File.join('packages', 'flutter_tools', 'bin', 'podhelper'), flutter_root)
